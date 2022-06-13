21cbf793822c6d977d61481660ab963fb9725bd0
Wassem Naddaf <wassemw71@gmail.com>
2022-06-13 23:43:59 +0200

    feat: add text filter in employees table

---
+++ app/core/filters/highlight.filter.js
@@ -0,0 +1,11 @@
+export default function highlight($sce) {
+  return function (text, phrase) {
+    if (phrase) {
+      text = text.replace(
+        new RegExp('(' + phrase + ')', 'gi'),
+        '<mark>$1</mark>'
+      );
+    }
+    return $sce.trustAsHtml(text);
+  };
+}
