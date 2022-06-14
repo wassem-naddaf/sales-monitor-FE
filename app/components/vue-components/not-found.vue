6f06c9a1d7d8b2a77f77b6d055f17d0886dc235c
Wassem Naddaf <wassemw71@gmail.com>
2022-06-14 14:23:12 +0200

    feat: add 404 not found page

---
+++ app/components/vue-components/not-found.vue
@@ -0,0 +1,7 @@
+<template>
+<div class="c-not-found">
+    <h1>404 Not Fount</h1>
+    <h2>Sorry, we can't find the page you are looking for</h2>
+    <a ui-sref="app">Back To Home</a>
+</div>
+</template>
---
+++ app/pages/not-found-page.vue
@@ -0,0 +1,14 @@
+<template>
+  <not-found-component />
+</template>
+
+<script>
+import NotFoundComponent from "../components/vue-components/not-found.vue";
+export default {
+  components: {
+    NotFoundComponent,
+  },
+};
+</script>
+
+<style></style>
